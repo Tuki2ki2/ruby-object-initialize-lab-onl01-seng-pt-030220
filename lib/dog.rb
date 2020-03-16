6aac8fde10b074c8ccb8fc105bd2c925feb0dfc8
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-16 18:02:30 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,6 @@
 class Dog
-  def initialize(name, breed)
+  def initialize(name, breed = "Mutt")
     @name = name
-    if breed == nil
-      @breed = "Mutt"
-      @breed
-    end
     @breed = breed
   end
 
