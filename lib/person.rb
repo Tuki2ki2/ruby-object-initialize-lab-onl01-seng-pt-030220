b5ab824675a373627003a5f25cc338d877efbc69
Erik Prenzlin <eprenzlin@gmail.com>
2020-03-16 17:27:31 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,14 @@
 class Person 
-  def Initialize(name)
-    @name = name  
+  def initialize(name)
+    @name = name
   end
-
   def name=(name)
     @name = name 
-    
   end
 
-def name 
-  @name
+  def name
+    @name
+  end
 end
 
-end
+Erik = Person.new ("Erik")
